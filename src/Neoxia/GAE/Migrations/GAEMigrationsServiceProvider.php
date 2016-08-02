4837103d9b0651243c287b563e641a7775d6ddbb
alexandre-butynski <alexandre.butynski@neoxia.com>
2016-08-02 19:49:13 +0200

    Fix GAEMigrationsServiceProvider unimplemented method

--- src/Neoxia/GAE/Migrations/GAEMigrationsServiceProvider.php
+++ src/Neoxia/GAE/Migrations/GAEMigrationsServiceProvider.php
@@ -6,15 +6,20 @@ use Illuminate\Support\ServiceProvider;
 
 class GAEMigrationsServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
     public function boot()
     {
         if (! $this->app->routesAreCached()) {
             require __DIR__.'/routes.php';
         }
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
     }
 }
