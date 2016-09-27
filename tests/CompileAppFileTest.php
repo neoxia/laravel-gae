4426bffce44cbc934d27300087a3e61b9cdb4d22
Thomas Ruiz <thomas.ruiz@neoxia.com>
2016-09-27 16:51:26 +0200

    Improve the test usability

--- tests/CompileAppFileTest.php
+++ tests/CompileAppFileTest.php
@@ -4,17 +4,36 @@ use Mockery as m;
 
 class CompileAppFileTest extends PHPUnit_Framework_TestCase
 {
+    /** @var m\Mock|\Neoxia\GAE\Console\CompileAppFile */
+    private $command;
+
+    /** @var m\Mock|\Illuminate\Config\Repository */
+    private $config;
+
+    /** @var m\Mock|\Illuminate\View\Compilers\BladeCompiler */
+    private $compiler;
+
+    /** @var m\Mock|\Illuminate\Filesystem\Filesystem */
+    private $files;
+
+    /** @var m\Mock|\Illuminate\View\View */
+    private $view;
+
+    /** @var m\Mock|\Illuminate\View\Factory */
+    private $viewFactory;
+
     protected function setUp()
     {
         parent::setUp();
 
-        $this->config = m::mock('Illuminate\Config\Repository');
-        $this->viewFactory = m::mock('Illuminate\View\Factory');
-        $this->compiler = m::mock('Illuminate\View\Compilers\BladeCompiler');
-        $this->files = m::mock('Illuminate\Filesystem\Filesystem');
-        $this->view = m::mock('Illuminate\View\View');
+        $this->config = m::mock(Illuminate\Config\Repository::class);
+        $this->viewFactory = m::mock(Illuminate\View\Factory::class);
+        $this->compiler = m::mock(Illuminate\View\Compilers\BladeCompiler::class);
+        $this->files = m::mock(Illuminate\Filesystem\Filesystem::class);
+        $this->view = m::mock(Illuminate\View\View::class);
 
-        $this->command = m::mock('Neoxia\GAE\Console\CompileAppFile[getView,info,error]', [$this->config, $this->files, $this->viewFactory, $this->compiler]);
+        $inject = [$this->config, $this->files, $this->viewFactory, $this->compiler];
+        $this->command = m::mock(Neoxia\GAE\Console\CompileAppFile::class . '[getView,info,error]', $inject);
         $this->command->shouldReceive('getView')->andReturn($this->view);
     }
 
