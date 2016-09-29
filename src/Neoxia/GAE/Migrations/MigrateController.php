941efc79a43c113d30b10ec788160b4611543f04
Thomas Ruiz <thomas.ruiz@neoxia.com>
2016-09-29 16:18:35 +0200

    Check database existence

--- src/Neoxia/GAE/Migrations/MigrateController.php
+++ src/Neoxia/GAE/Migrations/MigrateController.php
@@ -2,6 +2,8 @@
 
 namespace Neoxia\GAE\Migrations;
 
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
@@ -9,23 +11,49 @@ use Illuminate\Config\Repository as Config;
 
 class MigrateController extends BaseController
 {
-    public function post(Application $app, Request $request, Config $config)
+    public function post(Migrator $migrator, DatabaseManager $databaseManager, Request $request, Config $config)
     {
         if ($request->input('token') !== $config->get('app.admin_token')) {
             return response('Unauthorized', 401);
         }
 
-        $migrator = $app['migrator'];
-        $result = '';
+        $this->confirmDatabaseExistence($databaseManager, $config);
+        $result = 'Database existence confirmed.';
 
         if (! $migrator->repositoryExists()) {
             $migrator->getRepository()->createRepository();
             $result .= "Migration table created successfully.\n";
         }
 
-        $migrator->run($app->databasePath() . '/migrations');
+        $migrator->run(database_path() . '/migrations');
         $result .= str_replace(['<info>', '</info>'], '', implode("\n", $migrator->getNotes())) . "\n";
 
         return response($result, 200);
+    }
+
+    protected function confirmDatabaseExistence(DatabaseManager $databaseManager, Config $config)
+    {
+        $default = $config->get('database.default');
+        $database = $config->get('database.' . $default . '.database');
+        $config->set('database.' . $default . '.database', null);
+        $databaseManager->purge();
+        $connection = $databaseManager->connection($default);
+        $connection->statement("CREATE SCHEMA IF NOT EXISTS $database CHARSET utf8");
+        $databaseManager->purge();
+        $config->set('database.' . $default . '.database', $database);
+    }
+}
+
+if (! function_exists('database_path')) {
+    /**
+     * Get the path to the base of the install.
+     *
+     * @param  string $path
+     *
+     * @return string
+     */
+    function base_path($path = '')
+    {
+        return getcwd();
     }
 }
